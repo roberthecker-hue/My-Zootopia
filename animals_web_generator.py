0739fa8ada959ec4bf8356f96f46b6f68880d77e
Robert Hecker <robert.hecker@mac.com>
2026-09-24 14:03:59 +0200

    Erstes Feature frteig - Anzeigen alle Tiere aus JSON

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,9 +1,9 @@
 import json
 
 def load_data(file_path):
-  """ Loads a JSON file """
-  with open(file_path, "r") as handle:
-    return json.load(handle)
+    """ Loads a JSON file """
+    with open(file_path, "r") as handle:
+        return json.load(handle)
 
 
 
@@ -39,41 +39,33 @@ Type: mammal
 """
 def show_animal_information(animals_data):
     """ Shows information about animals """
+    for i in range(len(animals_data)):
+        # Name zuordnen und ausgeben
+        name = animals_data[i].get('name')
+        if name:
+            print(f"Name: {name}")
 
-    # Name zuordnen und ausgeben
-    name = animals_data[0].get('name')
-    if name:
-        print(f"Name: {name}")
+        # Sind Characteristics vorhanden? Dann Diet zuordnen und ausgeben
+        characteristics = animals_data[i].get('characteristics')
+        if characteristics:
+            diet = characteristics.get('diet')
+            if diet:
+                print(f"Diet: {diet}")
 
-    # Sind Characteristics vorhanden? Dann Diet zuordnen und ausgeben
-    characteristics = animals_data[0].get('characteristics')
-    if characteristics:
-        diet = characteristics.get('diet')
-        if diet:
-            print(f"Diet: {diet}")
+        # Sind Locations vorhanden und die Liste nicht None UND nicht eine leere Liste []?
+        # Dann ersten Ort aus Locations zuordnen und ausgeben
+        locations = animals_data[i].get('locations')
+        if locations:
+            erster_ort = locations[0]
+            print(f"Location: {erster_ort}")
 
-    # Sind Locations vorhanden und die Liste nicht None UND nicht eine leere Liste []?
-    # Dann ersten Ort aus Locations zuordnen und ausgeben
-    locations = animals_data[0].get('locations')
-    if locations:
-        erster_ort = locations[0]
-        print(f"Location: {erster_ort}")
+        # Sind Characteristics vorhanden? Dann Type zuordnen und ausgeben
+        if characteristics:
+            type = characteristics.get('type')
+            if type:
+                print(f"Type: {type}")
 
-    # Sind Characteristics vorhanden? Dann Type zuordnen und ausgeben
-    if characteristics:
-        type = characteristics.get('type')
-        if type:
-            print(f"Type: {type}\n")
-
-
-"""#    MEINE TESTS - DIE MÜSSEN NOCH ENTFERNT WERDEN
-    
-    print("\n=====================\n")
-
-    print(f"Animals_data:\n{animals_data[0]}")
-    print()
-    characteristics = animals_data[0].get("characteristics")
-    print(f"Characteristics: \n{characteristics}")"""
+        print()
 
 
 def main():
